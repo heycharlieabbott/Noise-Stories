5e9b20cbbe3807a8b9df108e5221ad710e76996b
unknown <chzabbott@gmail.com>
2021-07-03 00:13:14 +0900

    added noise, inverted camera, added box sdf

--- Assets/Materials/marcher.hlsl
+++ Assets/Materials/marcher.hlsl
@@ -3,7 +3,7 @@
 #define surfdist .1
 
 
-float SphereDist(float3 p, float3 ptwo, float rad, float3 modvec){
+float SphereDist(float3 p, float3 ptwo, float rad, float3 modvec, float3 cubesize){
 
       p.x = fmod(p.x, modvec.x) - modvec.x/2.;
       p.y = fmod(p.y, modvec.y) - modvec.y/2.;
@@ -12,11 +12,21 @@ float SphereDist(float3 p, float3 ptwo, float rad, float3 modvec){
 
  //p *= sin(p*(sin(ptwo.x/20.)));
   //p *= sin(p*(sin(ptwo.y/20.)));
+  
   p *= abs(sin(p*(sin(ptwo.y/20.)-2.)));
 
-  float d = length(p)/ rad;
+  ptwo = float3(0.,0.,0.);
 
-  d -= sin(p.x/4.)/1.2;
+  float sphere = length(p)/ rad;
+
+  float3 size = cubesize;
+  
+  float cube = length(max(abs(p)-size,0.));
+
+  float d = max(cube,sphere* sin(p));
+
+
+  //d -= sin(p.x/4.)/1.2;
 
   //d-= dot(p,d);
    
@@ -24,7 +34,7 @@ float SphereDist(float3 p, float3 ptwo, float rad, float3 modvec){
   }
  
  
- float RayMarch (float3 ro, float3 rd, float3 modvec, float rad){
+ float RayMarch (float3 ro, float3 rd, float3 modvec, float rad, float3 cubesize){
  float dO = 0.;
 //  float maxdist = 100.;
 //  int maxsteps = 50;
@@ -32,7 +42,7 @@ float SphereDist(float3 p, float3 ptwo, float rad, float3 modvec){
  
    for (int i=0; i<maxsteps; i++){
      float3 p = ro + rd * dO;
-     float dS = SphereDist(p, ro, rad, modvec);
+     float dS = SphereDist(p, ro, rad, modvec, cubesize);
      dO += dS;
      if(dO>maxdist || dS <surfdist) break;
    }
@@ -41,7 +51,7 @@ float SphereDist(float3 p, float3 ptwo, float rad, float3 modvec){
 
 
 
-void marcher_float (float3 campos, float3 rd, float2 uv, float3 modvec, float rad, float time, out float4 col, out float al){
+void marcher_float (float3 campos, float3 rd, float2 uv, float3 modvec, float rad, float time, float3 cubesize, float noise, float fogcontrol, float fogwarp, out float4 col, out float al){
     col = float4(0.,0.,0.,1.);
     al = 1.;
 
@@ -49,19 +59,20 @@ void marcher_float (float3 campos, float3 rd, float2 uv, float3 modvec, float ra
 
     rd.y *= -1.;
 
-    float3 camnorm = normalize(rd);
+    float3 camnorm = (rd);
 
     uv.xy -= 0.5;
 
     rd = normalize(float3(uv.x - camnorm.x,uv.y - camnorm.y,1));
 
-    float d = RayMarch(-campos + sin(time),rd, modvec, rad);
+    float d = RayMarch(campos,rd, modvec + noise, rad, cubesize);
 
-    float fog = 1-d/maxdist*2.;
+    float fog = 1-d/maxdist*fogcontrol;
 
     if (d < maxdist){
 
         col.rgb = 1.;
+        //col *= sin(fog * fogwarp);
         col *= fog;
         
     }
